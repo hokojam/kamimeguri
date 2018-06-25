c4d2e297d3ccdce498648f26f35a0e5e862ef448
Chen Rui <chenrui@chennomacbook-pro-23049.local>
2018-06-25 19:51:59 +0900

    Initial Commit

---
+++ kamimeguri/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  kamimeguri
+//
+//  Created by Chen Rui on 2018/06/25.
+//  Copyright © 2018 Chen Rui. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
